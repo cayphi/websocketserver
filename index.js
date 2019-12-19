b7e12286d1752e6c4e1772b7a49ede2430935062
cayphi <amd64vn@gmail.com>
2019-12-19 10:33:56 +0700

    19Dec2019-09

--- index.js
+++ index.js
@@ -182,7 +182,7 @@ wsServer.on('request', function(request) {
       const json = { 'deviceType': dataFromClient.deviceType };
       console.log('device Type: ' + dataFromClient.deviceType);
       console.log('type of dataFromClient.deviceType: ' + typeof(dataFromClient.deviceType));
-      console.log('type of typesDef.DEVICE: ' + typeof(typesDef.DEVICE);
+      console.log('type of typesDef.DEVICE: ' + typeof(typesDef.DEVICE));
 
       if (dataFromClient.deviceType === typesDef.INSTRUCTOR) {
         //TODO: process message from an instructor
